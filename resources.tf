e7e22bd5500b6ec7e2ba53b5705a22b5db4098c7
Arpit Rastogi <arprastogi@Arpits-MacBook-Air.local>
2019-02-09 15:06:51 +0530

    creating aws subnet

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "my_dev_network" {
     name = "devnetwork"
-    auto_create_subnetworks = true
+    auto_create_subnetworks = false
 }
 
 resource "aws_vpc" "env-example" {
@@ -10,4 +10,10 @@ resource "aws_vpc" "env-example" {
     tags {
       Name = "terraform-aws-vpc"
     }
+}
+
+resource "aws_subnet" "subnet1" {
+    cidr_block = "${cidrsubnet(aws_vpc.env-example.cidr_block, 3, 1)}"
+    vpc_id = "${aws_vpc.env-example.id}"
+    availability_zone = "us-east-1a"
 }
